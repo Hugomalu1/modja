cd397592f284a068cd8dd43f9465a1a1b91b2aae
Hugomalu1 <Markus@LAPTOP-EJKK0K67>
2024-02-29 18:28:24 +0100

    tt

---
+++ src/main/java/net/mcreator/jiojoi/procedures/RfrfProcedure.java
@@ -0,0 +1,10 @@
+package net.mcreator.jiojoi.procedures;
+
+import net.minecraft.core.Direction;
+
+public class RfrfProcedure {
+	public static void execute() {
+		for (Direction directioniterator : Direction.values()) {
+		}
+	}
+}
