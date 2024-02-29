20521ec77f15c4e8a19f6a267b9f71fbde694fc3
Hugomalu1 <Markus@LAPTOP-EJKK0K67>
2024-02-29 20:14:30 +0100

    k

--- src/main/java/net/mcreator/jiojoi/procedures/HhProcedure.java
+++ src/main/java/net/mcreator/jiojoi/procedures/HhProcedure.java
@@ -1,9 +1,17 @@
 package net.mcreator.jiojoi.procedures;
 
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
 
 public class HhProcedure {
-	public static void execute(ItemStack itemstack) {
+	public static void execute(Entity entity, ItemStack itemstack) {
+		if (entity == null)
+			return;
 		itemstack.grow(1);
+		if (entity instanceof Player _player) {
+			_player.getAbilities().mayfly = true;
+			_player.onUpdateAbilities();
+		}
 	}
 }
