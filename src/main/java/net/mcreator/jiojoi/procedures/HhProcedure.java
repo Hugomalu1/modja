67f88d411d08bdcd360870bf919b4a876c267f18
Hugomalu1 <Markus@LAPTOP-EJKK0K67>
2024-02-29 17:07:55 +0100

    iii

---
+++ src/main/java/net/mcreator/jiojoi/procedures/HhProcedure.java
@@ -0,0 +1,9 @@
+package net.mcreator.jiojoi.procedures;
+
+import net.minecraft.world.item.ItemStack;
+
+public class HhProcedure {
+	public static void execute(ItemStack itemstack) {
+		itemstack.grow(1);
+	}
+}
